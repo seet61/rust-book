1005c0413a8c90aaa7ab70824857caec9a757e4b
d.arefev <d.arefev@bft.ru>
2025-02-03 07:52:38 +0300

    first function

---
+++ src/chapter1/functions/target/debug/deps/functions-5c46dbf5dc850d18.d
@@ -0,0 +1,5 @@
+C:\Projects\rust_book\src\chapter1\functions\target\debug\deps\libfunctions-5c46dbf5dc850d18.rmeta: src\main.rs
+
+C:\Projects\rust_book\src\chapter1\functions\target\debug\deps\functions-5c46dbf5dc850d18.d: src\main.rs
+
+src\main.rs:
---
+++ src/chapter1/functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+C:\Projects\rust_book\src\chapter1\functions\target\debug\functions.exe: C:\Projects\rust_book\src\chapter1\functions\src\main.rs
