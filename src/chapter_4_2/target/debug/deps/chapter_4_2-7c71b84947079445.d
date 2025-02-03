26c733c82d6999cbb5ff0cc9c47b24c197240c17
d.arefev <d.arefev@bft.ru>
2025-02-03 14:45:13 +0300

    change directories names

---
+++ src/chapter_4_2/target/debug/deps/chapter_4_2-7c71b84947079445.d
@@ -0,0 +1,5 @@
+C:\Projects\rust_book\src\chapter_4_2\target\debug\deps\libchapter_4_2-7c71b84947079445.rmeta: src\main.rs
+
+C:\Projects\rust_book\src\chapter_4_2\target\debug\deps\chapter_4_2-7c71b84947079445.d: src\main.rs
+
+src\main.rs:
---
+++ src/chapter_4_2/target/debug/deps/chapter_4_2-7f6442d036293cbf.d
@@ -0,0 +1,5 @@
+C:\Projects\rust_book\src\chapter_4_2\target\debug\deps\libchapter_4_2-7f6442d036293cbf.rmeta: src\main.rs
+
+C:\Projects\rust_book\src\chapter_4_2\target\debug\deps\chapter_4_2-7f6442d036293cbf.d: src\main.rs
+
+src\main.rs:
