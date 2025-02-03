84fb73dbc68df75a441d74e103ddfbf614f647ca
d.arefev <d.arefev@bft.ru>
2025-02-03 08:54:10 +0300

    chapter 3 end

--- src/chapter1_3/functions/src/main.rs
+++ src/chapter1_3/functions/src/main.rs
@@ -27,7 +27,58 @@ fn main() {
         }
     };
 
-    print!("The result of loop is {result}");
+    println!("The result of loop is {result}");
+
+    println!("loop names");
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count ==2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    println!("While example");
+    let mut number = 3;
+    while number != 0 {
+        println!("{number}");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("The value is: {}", a[index]);
+
+        index += 1;
+    }
+
+    for element in a {
+        println!("The value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}");
+    }
+    println!("LIFTOFF!!!");
+
 }
 
 fn another_functin(x: i32) {
